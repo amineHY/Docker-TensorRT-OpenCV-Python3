c712d2abfb1692feab52927e29ce013fc564fb2b
Amine Hadj Youcef <amine.hadjyoucef@dronevolt.com>
2019-07-30 14:45:33 +0200

    update the runfile

--- docker_TensorRT_OpenCV_Python.sh
+++ docker_TensorRT_OpenCV_Python.sh
@@ -1,12 +1,13 @@
 
 if  [ "$1" == 'build' ]; then
 	echo "[info] Build a docker image from the Dockefile"
-	docker build -t aminehy/tensorrt-opencv-python3:v1.0 .
+	docker build -t aminehy/tensorrt-opencv-python3:v1.1 .
 elif [ "$1" == "push" ]; then
 	echo "[info] Push the docker image to docker hub"
-	docker push aminehy/tensorrt-opencv-python3:v1.0
+	docker push aminehy/tensorrt-opencv-python3:v1.1
 elif [ "$1" == "run" ]; then
 	echo "[info] Run the docker container"
 	xhost +
-	docker run -it --rm -v $(pwd):/workspace --runtime=nvidia -w /workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY aminehy/tensorrt-opencv-python3:v1.0 
+	docker run -it --rm -v $(pwd):/workspace --runtime=nvidia -w /workspace -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -p 8888:8888 aminehy/tensorrt-opencv-python3:v1.1
+		
 fi
